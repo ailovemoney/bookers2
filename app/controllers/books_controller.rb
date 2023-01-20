6d4b4e99f55f563fe08fe76792b1edbeee1107a8
ailovemoney <119666255+ailovemoney@users.noreply.github.com>
2023-01-20 11:56:23 +0000

    [Add]課題３

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new
+    @books = Book.all
   end
 
   def create
@@ -11,6 +12,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @books = Book.all
   end
 
   private
