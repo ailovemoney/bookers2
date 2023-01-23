6ccda1834ea395e3d7aa926ff17a8e8457e48054
ailovemoney <119666255+ailovemoney@users.noreply.github.com>
2023-01-23 13:49:34 +0000

    [Add]課題３

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,11 +8,28 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path(params[:book_id])
+    redirect_to book_path(@book.id)
   end
 
   def show
-    @books = Book.all
+    @book = Book.find(params[:id])
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @books = Book.find(params[:id])
+    @book = @user.books
   end
 
   def edit
@@ -18,7 +24,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
 end
