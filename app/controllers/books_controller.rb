5d4c2d6add4a298d8003ef33ddcad5f08d30bbeb
ailovemoney <119666255+ailovemoney@users.noreply.github.com>
2023-01-24 11:58:01 +0000

    [Add]課題３

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,13 +7,18 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
-    @book = Book.find(params[:id])
-    @user = User.find(params[:id])
+    @book = Book.new
+    @books = Book.find(params[:id])
   end
 
   def edit
@@ -21,9 +26,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,14 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @book = Book.new
   end
 
   def show
     @user = User.find(params[:id])
     @books = Book.find(params[:id])
-    @book = @user.books
+    @bookers = @user.books
+    @book = Book.new
   end
 
   def edit
@@ -16,8 +18,12 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      flash[:notice] = "successfully"
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
 
