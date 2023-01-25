91211efcbd9951c679e1d2db1618d44ef6087fd5
ailovemoney <119666255+ailovemoney@users.noreply.github.com>
2023-01-25 14:56:30 +0000

    [Add]課題３

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def index
     @book = Book.new
     @books = Book.all
@@ -45,6 +47,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 
 end
